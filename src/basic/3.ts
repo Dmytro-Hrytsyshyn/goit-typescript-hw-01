c2281779f78e653c4bd210a2c36e54be8375cd7f
Nataliia <nataliiaqa8@gmail.com>
2024-11-25 20:47:43 +0100

    add all

---
+++ src/basic/3.ts
@@ -0,0 +1,12 @@
+let mixedType: string | number;
+
+mixedType = "Name";
+mixedType = 2;
+
+type EnableOrDisable = "enable" | "disable";
+
+let enableOrDisable: EnableOrDisable;
+
+enableOrDisable = "disable";
+enableOrDisable = "enable";
+// enableOrDisable = "Hello";
